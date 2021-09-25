1e42a3239c0dfae6681909e241a19eb27fe4f7c4
Alex Ponomarev <ponomaa@alistr.com>
2021-09-24 20:30:35 -0400

    test

--- components/styles.js
+++ components/styles.js
@@ -1,6 +1,6 @@
-import styled from "styled-components";
+import styled from 'styled-components';
 
-import { StyleSheet } from "react-native";
+import { StyleSheet } from 'react-native';
 
 import {
   View,
@@ -10,16 +10,16 @@ import {
   TouchableOpacity,
   RecyclerViewBackedScrollViewComponent,
   EdgeInsetsPropType,
-} from "react-native";
+} from 'react-native';
 
-// colors
+// colors .
 export const Colors = {
-  primary: "#ffffff",
-  secondary: "#E5E7EB",
-  tertiary: "#1F2937",
-  darkLight: "#9CA3AF",
-  brand: "#6D28D9",
-  green: "#10B981",
+  primary: '#ffffff',
+  secondary: '#E5E7EB',
+  tertiary: '#1F2937',
+  darkLight: '#9CA3AF',
+  brand: '#6D28D9',
+  green: '#10B981',
   red: red,
 };
 
