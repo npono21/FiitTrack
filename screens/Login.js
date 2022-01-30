849e347f428ed03fa15e983195de7485b966eb1b
Nicholai Ponomarev <nickpon123@gmail.com>
2022-01-29 19:56:18 -0500

    New Pushes

--- screens/Login.js
+++ screens/Login.js
@@ -1,145 +1,119 @@
-import React, { useState } from "react";
-import { StatusBar } from "expo-status-bar";
-import { Formik } from "formik";
-import { ImageBackground, View, StyleSheet, Text } from "react-native";
-import { Octicons, Ionicons, Fontisto } from "@expo/vector-icons";
+import React, { useEffect } from "react";
 
 import {
-  InnerContainer,
-  SubTitle,
-  StyledFormArea,
-  LeftIcon,
-  StyledInputLabel,
-  StyledTextInput,
-  RightIcon,
-  Colors,
-  StyledButton,
-  ButtonText,
-  MsgBox,
-  Line,
-  ExtraText,
-  ExtraView,
-  TextLink,
-  TextLinkContent,
-} from "./../components/stylesLoginRegister";
-
-//Color Structuring
-const { brand, darkLight, primary } = Colors;
-
-//Keyboard avoiding wrapper.
-import KeyboardAvoidingWrapper from "../components/KeyboardAvoidingWrapper";
-
-import { commonAppStyles } from "../components/commonStylesApp";
-
-const styles = StyleSheet.create({
-  image: {
-    flex: 1,
-    width: "100%",
-    height: "100%",
-    justifyContent: "center",
-  },
-});
+  ImageBackground,
+  View,
+  StyleSheet,
+  Text,
+  Button,
+  TextInput,
+} from "react-native";
 
 const Login = ({ navigation }) => {
-  const [hidePassword, setHidePassword] = useState(true);
-
   return (
-    <KeyboardAvoidingWrapper>
-      <ImageBackground
-        blurRadius={10}
-        resizeMode="cover"
-        source={require("../assets/loginscreen.jpg")}
-        style={styles.image}
-        position="absolute"
+    <View style={{ backgroundColor: "black", flex: 1 }}>
+      <View style={{ marginTop: 100, marginLeft: 30 }}>
+        <Text
+          style={{
+            fontWeight: "bold",
+            fontSize: 55,
+            fontFamily: "System",
+            color: "white",
+          }}
+        >
+          FiitTrack
+        </Text>
+      </View>
+      <View
+        style={{
+          backgroundColor: "white",
+          justifyContent: "center",
+          alignSelf: "center",
+          width: 300,
+          padding: 15,
+          borderRadius: 25,
+          marginTop: 10,
+          marginTop: 75,
+        }}
       >
-        <View style={commonAppStyles.styledContainer}>
-          <StatusBar style="dark" />
-          <InnerContainer>
-            <Text style={commonAppStyles.loginRegsiterPageTitle}>
-              FiitTrack
-            </Text>
-            <SubTitle>Account Login</SubTitle>
-            <Formik
-              initialValues={{ email: "", password: "" }}
-              onSubmit={(values) => {
-                //console.log(values);
-                navigation.navigate("Workouts");
-              }}
-            >
-              {({ handleChange, handleBlur, handleSubmit, values }) => (
-                <StyledFormArea>
-                  <MyTextInput
-                    label="Email Address"
-                    icon="mail"
-                    placeholder="nickpon123@gmail.com"
-                    placeholderTextColor={darkLight}
-                    onChangeText={handleChange("email")}
-                    onBlur={handleBlur("email")}
-                    value={values.email}
-                    keyboardType="email-address"
-                  />
-                  <MyTextInput
-                    label="Password"
-                    icon="lock"
-                    placeholder="* * * * * * * *"
-                    placeholderTextColor={darkLight}
-                    onChangeText={handleChange("password")}
-                    onBlur={handleBlur("password")}
-                    value={values.password}
-                    secureTextEntry={hidePassword}
-                    isPassword={true}
-                    hidePassword={hidePassword}
-                    setHidePassword={setHidePassword}
-                  />
-                  <MsgBox>...</MsgBox>
-                  <StyledButton onPress={handleSubmit}>
-                    <ButtonText>Login</ButtonText>
-                  </StyledButton>
-                  <Line />
-                  <StyledButton google={true} onPress={handleSubmit}>
-                    <Fontisto name="google" color={primary} size={25} />
-                    <ButtonText google={true}>Sign in with Google</ButtonText>
-                  </StyledButton>
-                  <ExtraView>
-                    <ExtraText>Don't have an account already? </ExtraText>
-                    <TextLink onPress={() => navigation.navigate("Signup")}>
-                      <TextLinkContent>Signup</TextLinkContent>
-                    </TextLink>
-                  </ExtraView>
-                </StyledFormArea>
-              )}
-            </Formik>
-          </InnerContainer>
-        </View>
-      </ImageBackground>
-    </KeyboardAvoidingWrapper>
-  );
-};
-
-const MyTextInput = ({
-  label,
-  icon,
-  isPassword,
-  hidePassword,
-  setHidePassword,
-  ...props
-}) => {
-  return (
-    <View>
-      <LeftIcon>
-        <Octicons name={icon} size={30} color="red" />
-      </LeftIcon>
-      <StyledInputLabel> {label} </StyledInputLabel>
-      <StyledTextInput {...props} />
-      {isPassword && (
-        <RightIcon onPress={() => setHidePassword(!hidePassword)}>
-          <Ionicons
-            name={hidePassword ? "md-eye-off" : "md-eye"}
-            size={30}
-            color="red"
-          />
-        </RightIcon>
-      )}
+        <TextInput
+          style={{
+            fontSize: 17,
+            textAlign: "left",
+            color: "black",
+            fontFamily: "System",
+          }}
+          placeholder="Email"
+          placeholderTextColor={"lightgrey"}
+        />
+      </View>
+      <View
+        style={{
+          backgroundColor: "white",
+          justifyContent: "center",
+          alignSelf: "center",
+          width: 300,
+          padding: 15,
+          borderRadius: 25,
+          marginTop: 17,
+        }}
+      >
+        <TextInput
+          style={{
+            fontSize: 17,
+            textAlign: "left",
+            color: "black",
+            fontFamily: "System",
+          }}
+          placeholder="Password"
+          placeholderTextColor={"lightgrey"}
+        />
+      </View>
+      <View
+        style={{
+          backgroundColor: "white",
+          justifyContent: "center",
+          alignContent: "center",
+          alignSelf: "center",
+          width: 300,
+          height: 50,
+          borderRadius: 25,
+          marginTop: 17,
+          backgroundColor: "navy",
+        }}
+      >
+        <Button
+          title="Sign In"
+          onPress={() => navigation.navigate("Workouts")}
+          color="white"
+        />
+      </View>
+      <View></View>
+      <View>
+        <Button title="Forgot Password?" color="white" />
+      </View>
+      <View
+        style={{
+          flexDirection: "row",
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+      >
+        <Text
+          style={{
+            fontSize: 17,
+            color: "grey",
+            fontFamily: "System",
+          }}
+        >
+          Don't have an account?
+        </Text>
+        <Button
+          title="Sign Up"
+          color="lightblue"
+          onPress={() => navigation.navigate("Signup")}
+        />
+      </View>
     </View>
   );
 };
